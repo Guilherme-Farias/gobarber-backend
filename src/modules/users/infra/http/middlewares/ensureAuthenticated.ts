121f0b5d49519e6a1c12bcbe49d733934eb2c59b
Guilherme Farias <guicfarias11@gmail.com>
2021-01-25 16:29:55 -0300

    Separando em módulos utilizando a metodologia DDD

--- src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
+++ src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
-import authConfig from '../config/auth';
-import AppError from '../errors/AppError';
+import authConfig from '@config/auth';
+import AppError from '@shared/errors/AppError';
 
 interface TokenPayload {
   iat: number;
@@ -17,7 +17,7 @@ export default function ensureAuthenticated(
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new AppError({ message: 'JWT token is missing', statusCode: 401 });
+    throw new AppError('JWT token is missing', 401);
   }
 
   const [, token] = authHeader.split(' ');
@@ -28,6 +28,6 @@ export default function ensureAuthenticated(
 
     return next();
   } catch {
-    throw new AppError({ message: 'Invalid JWT token', statusCode: 401 });
+    throw new AppError('Invalid JWT token', 401);
   }
 }
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,15 +3,15 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
-import uploadConfig from './config/upload';
 
-import './database';
-import AppError from './errors/AppError';
+import '@shared/infra/typeorm';
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
@@ -26,7 +26,8 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 
   return response.status(500).json({
     status: 'error',
-    message: 'Internal server error',
+    // message: 'Internal server error',
+    message: `${err}`,
   });
 });
 

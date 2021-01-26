fae77700c9287524a123857841ef5c6ba9d9533a
Guilherme Farias <guicfarias11@gmail.com>
2021-01-26 12:58:30 -0300

    Criando o primeiro teste

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,31 @@
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import { v4 as uuid } from 'uuid';
+
+class AppointmentsRepository implements IAppointmentRepository {
+  private appointments: Appointment[] = [];
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), date, provider_id });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+
+    return findAppointment;
+  }
+}
+
+export default AppointmentsRepository;
